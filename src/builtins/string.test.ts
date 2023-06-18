2080120955acb243f47f6f1d3799c3d4a607b60c
Aaron Pettengill <pettengill.aaron@gmail.com>
2023-06-18 10:42:51 -0400

    test: add more error message assertions

--- src/builtins/string.test.ts
+++ src/builtins/string.test.ts
@@ -36,13 +36,29 @@ it("should fail for non-strings", () => {
 });
 
 it("should validate min length", () => {
+  expectType<string>(e().string().minChars(5).validate("foo123")).toEqual(
+    "foo123"
+  );
+  expect(() => e().label("Foo").string().minChars(5).validate("foo")).toThrow(
+    "Foo must be at least 5 characters"
+  );
   expect(() => e().string().minChars(5).validate("foo")).toThrow(
     "Must be at least 5 characters"
+  );
+  expect(() => e().string().minChars(5, "Custom").validate("foo")).toThrow(
+    "Custom"
   );
 });
 
 it("should validate max length", () => {
+  expectType<string>(e().string().maxChars(5).validate("foo")).toEqual("foo");
+  expect(() =>
+    e().label("Foo").string().maxChars(5).validate("foo123")
+  ).toThrow("Foo must be no more than 5 characters");
   expect(() => e().string().maxChars(5).validate("foo123")).toThrow(
     "Must be no more than 5 characters"
+  );
+  expect(() => e().string().maxChars(5, "custom").validate("foo123")).toThrow(
+    "custom"
   );
 });
